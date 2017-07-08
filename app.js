f3b386dc3ed392067281c94db082ed62d67a7214
Clinton Dreisbach <clinton@dreisbach.us>
2017-07-08 15:25:08 -0400

    Add mustache

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
-
+const path = require('path')
+const mustacheExpress = require('mustache-express')
 const app = express()
 
+app.engine('mustache', mustacheExpress())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.set('layout', 'layout')
+app.use('/static', express.static('static'))
 
 if (require.main === module) {
     app.listen(3000, function () {
